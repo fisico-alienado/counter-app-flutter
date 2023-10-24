2275f7360c2b78b607a78f43eeb098bc16626e26
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-10-24 11:14:42 +0200

    Estructura de directorios - Añadir nuevas pantallas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/screens/counters/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,8 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner:
             false, // quita la etiqueta "Debug" que aparece en la esquina superior derecha
-        home: Scaffold(
-          body: Center(child: Text('Hola Mundo')),
-        ));
+        home: CounterScreen());
   }
 }
