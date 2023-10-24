57474944ea0f542c7082e47ffe6b9866c8f0f94b
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-10-24 13:18:50 +0200

    Stateful Widget y como cambiar el estado de la aplicacion

--- lib/presentation/screens/counters/counter_screen.dart
+++ lib/presentation/screens/counters/counter_screen.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Counter Screen')),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-            Text('Cantidad de clicks', style: TextStyle(fontSize: 25)),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
+  State<CounterScreen> createState() => _CounterScreenState();
+}
 
-        },
-        child: const Icon(Icons.plus_one),
-      )
-    );
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Counter Screen')),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$clickCounter',
+                  style: const TextStyle(
+                      fontSize: 160, fontWeight: FontWeight.w100)),
+              const Text('Cantidad de clicks', style: TextStyle(fontSize: 25)),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {            
+            clickCounter += 1;
+            setState(() {}); // FUNCION QUE ACTUALIZA LOS WIDGETS, QUE REENDERIZA LOS WIDGETS de nuevo
+            // Otra forma valida
+            /*
+                        setState(() {
+              clickCounter += 1;
+            });
+            */
+          },
+          child: const Icon(Icons.plus_one),
+        ));
   }
 }
